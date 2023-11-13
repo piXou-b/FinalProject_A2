fd87a592b712b6f7e8cd556496a3bc4ad53e07c0
Benjamin Emily <benjaminemily@Benjamins-MacBook-Pro.local>
2023-11-13 18:19:51 +0100

    test

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//git add . pour tout mettre pret au commit au lieu de faire file par file
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! yop");
