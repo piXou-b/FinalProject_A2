a0e9b77663c1f6cea166540f8abb6d7d5184ddf5
benjamin Emily <benji.emily@icloud.com>
2023-11-27 20:29:55 +0100

    Ajout class dico

---
+++ FinalProject/Dictionnaire.cs
@@ -0,0 +1,13 @@
+namespace FinalProject;
+
+public class Dictionnaire
+{
+    private string _language;
+    
+    public Dictionnaire(string language)
+    {
+        this._language = language;
+    } 
+    
+    
+}
