ac7635b1519aa3e87223a172900f330360679c8a
benjamin Emily <benji.emily@icloud.com>
2023-11-28 17:57:41 +0100

    Anvancé class dico

--- FinalProject/Dictionnaire.cs
+++ FinalProject/Dictionnaire.cs
@@ -1,13 +1,36 @@
 namespace FinalProject;
+using System.IO;
 
 public class Dictionnaire
 {
     private string _language;
+    private List<string> dictionnaire;
     
     public Dictionnaire(string language)
     {
         this._language = language;
-    } 
-    
-    
+        this.dictionnaire = new List<string>();
+    }
+
+    public string toString()
+    {
+        string line = "";
+        try
+        {
+            StreamReader sr = new StreamReader("Final_Project/FinalProject/Mots_Français.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+        return line;
+    }
 }
