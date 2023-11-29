dde01216373c843941b924d721f0fe2bea7a1028
benjamin Emily <benji.emily@icloud.com>
2023-11-29 21:40:34 +0100

    Ajout class Plateau et fin de method toString et generateRndPlateu

---
+++ FinalProject/Plateau.cs
@@ -0,0 +1,102 @@
+namespace FinalProject;
+using System;
+using System.Text;
+
+public class Plateau
+{
+    private string[,] _plateau;
+
+    public Plateau()
+    {
+        this._plateau = new string[8, 8];
+    }
+
+    public string[,] SetRndPlateau()
+    {
+        try
+        {
+            List<int> occ = new List<int>();
+            List<int> occLatente = Enumerable.Repeat(0, 26).ToList();
+            Random rnd = new Random();
+            
+            string letter;
+            int i = 0;
+            StreamReader plateau = new StreamReader("files/Lettre.txt");
+            while ((letter = plateau.ReadLine()) != null)
+            {
+                string[] parts = letter.Split(',');
+                
+                if (parts.Length == 3 && int.TryParse(parts[1], out int occurrence))
+                {
+                    occ.Add(occurrence);
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine($"Format de ligne incorrect à la ligne {i + 1}. Ignoré.");
+                }
+            }
+            plateau.Close();
+            
+            for (int x = 0; x < this._plateau.GetLength(0); x++)
+            {
+                for (int j = 0; j < this._plateau.GetLength(1); j++)
+                {
+                    do
+                    {
+                        int randomIndex = rnd.Next(26);
+                        char randomLetter = (char)('A' + randomIndex);
+                        
+                        if (occLatente[randomLetter - 'A'] < occ[randomLetter - 'A'])
+                        {
+                            this._plateau[x, j] = randomLetter.ToString();
+                            occLatente[randomLetter - 'A']++;
+                            break;
+                        }
+                    } while (true);
+                }
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        
+        return this._plateau;
+    }
+
+    public string toString()
+    {
+        if (this._plateau.Length == 0)
+        {
+            return "La matrice est vide.";
+        }
+        
+        string result = "";
+
+        for (int i = 0; i < _plateau.GetLength(0); i++)
+        {
+            for (int j = 0; j < _plateau.GetLength(1); j++)
+            {
+                result += this._plateau[i, j];
+
+                if (j < _plateau.GetLength(1) - 1)
+                {
+                    result += ", ";
+                }
+            }
+
+            result += Environment.NewLine;
+        }
+
+        return result;
+    }
+}
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,17 +1,21 @@
 ﻿using FinalProject;
 
-string name = "benjamin";
-string mot = "kaka";
-Joueur joueur = new Joueur(name);
-
-joueur.Add_Mot(mot);
-Console.WriteLine(joueur.Contient(mot));
-Console.WriteLine(joueur.toString());
+// string name = "benjamin";
+// string mot = "kaka";
+// Joueur joueur = new Joueur(name);
+//
+// joueur.Add_Mot(mot);
+// Console.WriteLine(joueur.Contient(mot));
+// Console.WriteLine(joueur.toString());
+//
+// Console.WriteLine();
+// Dictionnaire dico = new Dictionnaire();
+// Console.WriteLine(dico.toString());
+// string str = "SE";
+// Console.WriteLine(dico.RechDicoRecursif(str.ToUpper(), 0, dico.Dico.Count-1));
 
 Console.WriteLine();
-Dictionnaire dico = new Dictionnaire();
-Console.WriteLine(dico.toString());
-string str = "ANALOGIES";
-Console.WriteLine(dico.RechDicoRecursif(str.ToUpper(), 0, dico.Dico.Count-1));
-
+Plateau plateau = new Plateau();
+plateau.SetRndPlateau();
+Console.WriteLine(plateau.toString());
 
