e9d619220561d02ba06693b13a375c9ad8f4d209
Dorian Boureau <dorian.boureau@edu.devinci.fr>
2023-12-05 17:04:23 +0100

    ajout class jeu

---
+++ FinalProject/Jeu.cs
@@ -0,0 +1,20 @@
+﻿namespace FinalProject;
+
+public class Jeu
+{
+    private Dictionnaire _dico;
+    private Plateau _plateau;
+    private Joueur _joueur1;
+    private Joueur _joueur2;
+    private TimeSpan _gametime;
+
+    public Jeu(Dictionnaire _dico, Plateau _plateau, Joueur _joueur1, Joueur _joueur2, TimeSpan _gametime)
+    {
+        this._dico = _dico;
+        this._plateau = _plateau;
+        this._joueur1 = _joueur1;
+        this._joueur2 = _joueur2;
+        this._gametime = _gametime;
+    }
+    
+}
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -23,10 +23,77 @@
 // plateau.ToFile("files/" + fileName);
 
 Plateau plateau2 = new Plateau("files/Test1.csv");
-Console.WriteLine(plateau2.toString());
-Console.WriteLine("Entrez un mot");
-string mot = Console.ReadLine();
-plateau2.Recherche_Mot(mot);
-Console.WriteLine(plateau2.toString());
-plateau2.MajPlateau();
-Console.WriteLine(plateau2.toString());
+// Console.WriteLine(plateau2.toString());
+// Console.WriteLine("Entrez un mot");
+// string mot = Console.ReadLine();
+// plateau2.Recherche_Mot(mot);
+// Console.WriteLine(plateau2.toString());
+// plateau2.MajPlateau();
+// Console.WriteLine(plateau2.toString());
+
+Joueur joueur1 = new Joueur("Dorian");
+Joueur joueur2 = new Joueur("Benjamin");
+Dictionnaire dico = new Dictionnaire();
+TimeSpan gametime = new TimeSpan();
+
+ConsoleKeyInfo cki;
+Jeu jeu = null;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Choisissez une durée de partie :");
+    Console.WriteLine("1. 2 minutes");
+    Console.WriteLine("2. 2 minutes 30 secondes");
+    Console.WriteLine("3. 3 minutes");
+    Console.WriteLine("4. 3 minutes 30 secondes");
+    Console.WriteLine("5. 4 minutes");
+    Console.WriteLine("6. 4 minutes 30 secondes");
+    Console.WriteLine("7. 5 minutes");
+    cki = Console.ReadKey(true);
+
+    switch (cki.Key)
+    {
+        
+
+        case ConsoleKey.D1:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(120.0));
+            Console.WriteLine("1");
+            break;
+        case ConsoleKey.D2:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(150.0));
+            Console.WriteLine("2");
+            break;
+        case ConsoleKey.D3:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(180.0));
+            Console.WriteLine("3");
+            break;
+        case ConsoleKey.D4:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(210.0));
+            Console.WriteLine("4");
+            break;
+        case ConsoleKey.D5:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(240.0));
+            Console.WriteLine("5");
+            break;
+        case ConsoleKey.D6:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(270.0));
+            Console.WriteLine("6");
+            break;
+        case ConsoleKey.D7:
+            jeu = new Jeu(dico, plateau2, joueur1, joueur2, TimeSpan.FromSeconds(300.0));
+            Console.WriteLine("7");
+            break;
+        
+            
+        default:
+            Console.WriteLine("Touche non valide. Veuillez choisir une touche correspondant à un temps indiqué");
+            break;
+    }
+
+} while (cki.Key != ConsoleKey.Escape);
+
+if (jeu != null)
+{
+    Console.WriteLine(jeu);
+}
