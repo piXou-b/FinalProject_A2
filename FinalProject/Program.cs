0875d2498f245d73a5367fe5dcc0784568e3ed6c
benjamin Emily <benji.emily@icloud.com>
2023-12-05 16:59:17 +0100

    nUnit test done for Joueur and Dictionnaire + fix bug filepath

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -8,11 +8,12 @@
 // Console.WriteLine(joueur.Contient(mot));
 // Console.WriteLine(joueur.toString());
 //
-// Console.WriteLine();
-// Dictionnaire dico = new Dictionnaire();
-// Console.WriteLine(dico.toString());
-// string str = "SE";
-// Console.WriteLine(dico.RechDicoRecursif(str.ToUpper(), 0, dico.Dico.Count-1));
+Console.WriteLine();
+string cheminFichier = Path.Combine("..", "..", "..", "..", "data", "Mots_Français.txt");
+Dictionnaire dico = new Dictionnaire(cheminFichier);
+Console.WriteLine(dico.toString());
+string str = "SE";
+Console.WriteLine(dico.RechDicoRecursif(str.ToUpper(), 0, dico.Dico.Count-1));
 
 // Console.WriteLine();
 // Plateau plateau = new Plateau();
@@ -22,11 +23,11 @@
 // string fileName = "Saved_Plateau_" + uniqueId;
 // plateau.ToFile("files/" + fileName);
 
-Plateau plateau2 = new Plateau("files/Test1.csv");
-Console.WriteLine(plateau2.toString());
-Console.WriteLine("Entrez un mot");
-string mot = Console.ReadLine();
-plateau2.Recherche_Mot(mot);
-Console.WriteLine(plateau2.toString());
-plateau2.MajPlateau();
-Console.WriteLine(plateau2.toString());
+// Plateau plateau2 = new Plateau("files/Test1.csv");
+// Console.WriteLine(plateau2.toString());
+// Console.WriteLine("Entrez un mot");
+// string mot = Console.ReadLine();
+// plateau2.Recherche_Mot(mot);
+// Console.WriteLine(plateau2.toString());
+// plateau2.MajPlateau();
+// Console.WriteLine(plateau2.toString());
